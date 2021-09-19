4ae3921ecf91df57059d49b94b6ea7c8118a533d
ienaga <ienaga@tvon.jp>
2021-09-19 22:54:49 +0900

    added 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # env
 Next2D Framework env function.
+
+```linux
+npm i -D @next2d/env
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function ()
+{
+    const args = process.argv.slice(2);
+    for (let idx = 0; args.length > idx; ++idx) {
+
+        const value = args[idx]
+        if (value.indexOf("env") === -1) {
+            continue;
+        }
+
+        return value.split("=")[1];
+    }
+
+    return "local";
+}
